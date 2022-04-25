3139112a7324999d508996c49c4f57011199f024
Kevin Renskers <kevin@loopwerk.io>
2022-04-25 22:31:52 +0200

    Re-render docs, they should have stable diffs now

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
+# Pretty print DocC JSON output so that it can be consistently diffed between commits
+export DOCC_JSON_PRETTYPRINT="YES"
+
 # See https://apple.github.io/swift-docc-plugin/documentation/swiftdoccplugin/publishing-to-github-pages
-swift package --allow-writing-to-directory ./docs generate-documentation --target Saga --output-path ./docs --transform-for-static-hosting --hosting-base-path Saga
+swift package \
+  --allow-writing-to-directory ./docs \
+  generate-documentation \
+  --target Saga \
+  --output-path ./docs \
+  --transform-for-static-hosting \
+  --hosting-base-path Saga
