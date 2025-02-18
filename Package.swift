e477e33e65b0ace197ee4e94e599dbe215444c47
Kevin Renskers <kevin@loopwerk.io>
2025-02-18 21:33:55 +0100

    chore: Rerender docs

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/kylef/PathKit", from: "1.0.1"),
-    // .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
+    //.package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
   ],
   targets: [
     .target(
