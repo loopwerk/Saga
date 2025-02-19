bc4d4110429e4f8c895d629aee75fbdb7d62bda0
Kevin Renskers <kevin@loopwerk.io>
2025-02-19 18:02:14 +0100

    chore: Easier way to generate the documentation. Have the Swift Package Index use my documentation, instead of the docs living in multiple places.
    SPI is hosting docs for a rather old version of Saga!

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
+set -e
+
+PACKAGE_SWIFT="Package.swift"
+DOCC_PLUGIN_LINE=".package(url: \"https://github.com/swiftlang/swift-docc-plugin\", from: \"1.1.0\"),"
+
+# Enable the swift-docc-plugin dependency
+sed -i '' "s|//\s*$DOCC_PLUGIN_LINE|$DOCC_PLUGIN_LINE|" "$PACKAGE_SWIFT"
+
 # Pretty print DocC JSON output so that it can be consistently diffed between commits
 export DOCC_JSON_PRETTYPRINT="YES"
 
-# See https://swiftlang.github.io/swift-docc-plugin/documentation/swiftdoccplugin/publishing-to-github-pages
+# Generate documentation
 swift package \
   --allow-writing-to-directory ./docs \
   generate-documentation \
@@ -12,3 +20,10 @@ swift package \
   --output-path ./docs \
   --transform-for-static-hosting \
   --hosting-base-path Saga
+
+# Restore Package.swift by commenting out the dependency again
+if grep -q "$DOCC_PLUGIN_LINE" "$PACKAGE_SWIFT"; then
+  sed -i '' "s|$DOCC_PLUGIN_LINE|//$DOCC_PLUGIN_LINE|" "$PACKAGE_SWIFT"
+fi
+
+echo "Documentation generated successfully."
